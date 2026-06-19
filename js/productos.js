990777574b0b457d10ba18706f3d4484685202fa
Daniel Steven Lopez Perez <dast11lp@gmail.com>
2026-06-19 14:22:18 -0500

    Revert "carrito service"
    This reverts commit f945ead6c863770173d536bc604d0663400fb077.

--- js/productos.js
+++ js/productos.js
@@ -1,27 +1,22 @@
-import { getProductos } from "../services/productService.js";
-import { agregarAlCarrito } from "../services/cartService.js";
-
-let productos = []
-
-
-
 async function obtenerProductos(...categorias) {
-
+    
     document.getElementById("productos").innerHTML = "";
-    productos = [];
 
     for (const categoria of categorias) {
-        const productosCategoria = await getProductos(categoria);
-        productos.push(...productosCategoria);
-
-        productosCategoria.forEach(producto => {
+        const respuesta = await fetch('https://dummyjson.com/products/category/' + categoria + '?limit=50');
+        const datos = await respuesta.json();
+        
+        const productos = datos.products;
+        console.log(productos);
+        
+        productos.forEach(producto => {
             document.getElementById("productos").innerHTML += `
             <div class="card" style="width: 18rem;">
                 <img src="${producto.thumbnail}" class="card-img-top" alt="${producto.title}">
                 <div class="card-body">
                     <h5 class="card-title">${producto.title}</h5>
                     <p class="card-text">${producto.description}</p>
-                    <button data-id="${producto.id}" class="btn btn-primary agregar-carrito">Agregar al carrito</button>
+                    <a href="#" class="btn btn-primary">Agregar al carrito</a>
                 </div>
             </div>
             `;
@@ -29,39 +24,31 @@ async function obtenerProductos(...categorias) {
     }
 }
 
-document.getElementById("productos").addEventListener("click", function (e) {
-    if (e.target.classList.contains("agregar-carrito")) {
-        const id = Number(e.target.dataset.id);
-        const producto = productos.find(p => p.id === id);
-        if (producto) {
-            agregarAlCarrito(producto);
-        }
-    }
-});
-
-function obtenerProductosHombre() {
+function obtenerProductosHombre ()
+ {
     document.getElementById("Hombre")
         .addEventListener("click", function () {
             obtenerProductos("mens-shirts", "mens-shoes", "mens-watches");
         });
 }
 
-function obtenerProductosMujer() {
+function obtenerProductosMujer ()
+ {
     document.getElementById("Mujer")
         .addEventListener("click", function () {
             obtenerProductos("womens-dresses", "tops", "womens-shoes", "womens-watches");
         });
 }
 
-function obtenerProductosJewelery() {
+function obtenerProductosJewelery ()
+ {
     document.getElementById("Accesorios")
         .addEventListener("click", function () {
             obtenerProductos("womens-jewellery", "womens-bags", "sunglasses");
         });
-}
+}   
 
 obtenerProductosHombre();
 obtenerProductosMujer();
 obtenerProductosJewelery();
 
-export { productos };
--- services/productService.js
+++
@@ -1,6 +0,0 @@
-export const getProductos  = async (categoria) => {
-    const respuesta = await fetch('https://dummyjson.com/products/category/' + categoria + '?limit=50');
-    const datos = await respuesta.json();
-    const productos = datos.products;
-    return productos;
-}
